aa8da94265e0faec70d25faee371612d9afd6218
Jaci Brunning <jaci.brunning@gmail.com>
2018-12-30 19:46:19 +0800

    Fix bootstrap again, with new user

--- vision/bootstrap.sh
+++ vision/bootstrap.sh
@@ -4,10 +4,16 @@
 sudo apt-get update
 sudo apt-get install -y avahi-daemon libnss-mdns v4l-utils
 
+# Add vision user and password
+sudo adduser vision --disabled-password --gecos ""
+sudo adduser vision sudo
+sudo usermod --password curtinfrc vision
+
+# Enable password-less sudo for everyone
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+
 # Set hostname, user and password
 echo curtinvision | sudo tee /etc/hostname
-sudo usermod -l curtinfrc linaro
-sudo usermod --password curtinfrc curtinfrc
 
 # Reboot to commit changes
 sudo reboot
