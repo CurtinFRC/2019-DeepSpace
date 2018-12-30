1124790e701644b81fc96bebb0dff392cdfb4692
Jaci Brunning <jaci.brunning@gmail.com>
2018-12-30 20:05:29 +0800

    Add vision bootstrap scripts

--- vision/bootstrap.sh
+++ vision/bootstrap.sh
@@ -2,15 +2,15 @@
 
 # Install required packages
 sudo apt-get update
-sudo apt-get install -y avahi-daemon libnss-mdns v4l-utils
+sudo apt-get install -y avahi-daemon libnss-mdns v4l-utils imagemagick
+
+# Enable password-less sudo for everyone
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 
 # Add vision user and password
 sudo adduser vision --disabled-password --gecos ""
 sudo adduser vision sudo
-sudo usermod --password curtinfrc vision
-
-# Enable password-less sudo for everyone
-echo "%sudo ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+echo "vision:curtinfrc" | sudo chpasswd
 
 # Set hostname, user and password
 echo curtinvision | sudo tee /etc/hostname
