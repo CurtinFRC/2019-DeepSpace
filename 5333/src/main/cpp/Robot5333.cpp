e35e75416b439a0c69903a95b2c6deec23ac4d82
Joseph Loveday <joseph.loveday2@gmail.com>
2019-01-03 15:00:02 +0800

    Initial drive code in the cpp and header

--- 5333/src/main/cpp/Robot5333.cpp
+++ 5333/src/main/cpp/Robot5333.cpp
@@ -3,15 +3,40 @@
 #include <math.h>
 #include <iostream>
 
-#include "frc/Timer.h"
+using namespace frc;
+using namespace curtinfrc;
 
-void Robot::RobotInit() { }
+void Robot::RobotInit() {
+  Xbox = new XboxController(0);
+  Xbox2 = new XboxController(1);
+  
+  LeftMotors[0] = new talon_srx(0);
+  LeftMotors[0]->SetInverted(false);
+  LeftMotors[1] = new talon_srx(1);
+  LeftMotors[1]->SetInverted(false);
+  LeftMotors[1]->Set(talon_srx::control_mode::Follower, LeftMotors[0]->get_port());
 
-void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {}
+  RightMotors[0] = new talon_srx(2);
+  RightMotors[0]->SetInverted(true);
+  RightMotors[1] = new talon_srx(3);
+  RightMotors[1]->SetInverted(true);
+  RightMotors[1]->Set(talon_srx::control_mode::Follower, RightMotors[0]->get_port());
+};
 
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() { }
+void Robot::AutonomousInit() {};
+void Robot::AutonomousPeriodic() {};
 
-void Robot::TestInit() {}
-void Robot::TestPeriodic() {}
+void Robot::TeleopInit() {};
+void Robot::TeleopPeriodic() {
+  double leftSpeed = -Xbox->GetY(Xbox->kLeftHand);
+  double rightSpeed = -Xbox->GetY(Xbox->kRightHand);
+
+  leftSpeed *= abs(leftSpeed);
+  rightSpeed *= abs(rightSpeed);
+
+  LeftMotors[0]->Set(leftSpeed);
+  RightMotors[0]->Set(rightSpeed);
+};
+
+void Robot::TestInit() {};
+void Robot::TestPeriodic() {};
