7166debfa79be110dd991fae32fe6b6cefac11b2
Sonny6155 <songyi.loo@gmail.com>
2019-01-09 10:02:39 +0800

    Added SensorTransmission (untested)

---
+++ common/src/main/include/SensoredTransmission.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <frc/SpeedController.h>
+#include <sensors/Encoder.h>
+
+/* Combines the encoder and motor parts into one data structure,
+so that both Spark + encoder and Talon SRX can be treated the same. */
+
+namespace curtinfrc {
+  class SensoredTransmission {
+   public:
+    // Passing _transmission and _encoder
+    SensoredTransmission(frc::SpeedController &_transmission, sensors::Encoder &_encoder;) : transmission(_transmission), encoder(_encoder) {};
+    frc::SpeedController &transmission;
+    sensors::Encoder &encoder;
+  };
+}
