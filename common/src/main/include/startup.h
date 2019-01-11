4c46199b0da65dfe50946701cfd3f38b9f4a47c9
N.R.T <nathsharwor@hotmail.com>
2019-01-11 11:30:30 +0800

    Rename startup to lowercase

--- common/src/main/include/startup.h
+++ common/src/main/include/startup.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <frc/RobotBase.h>
 
 namespace curtinfrc {
--- common/src/simulation/cpp/simulation_startup.cpp
+++ common/src/simulation/cpp/simulation_startup.cpp
@@ -1,5 +1,5 @@
 #include "simulation/simulation.h"
-#include "Startup.h"
+#include "startup.h"
 
 void curtinfrc::RobotStartup::Start(std::function<int()> robotFunc) {
   static simulation::harness sim_harness;
