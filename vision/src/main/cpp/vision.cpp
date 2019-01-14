d5b56d5881662203345d988427b7805ad3eab631
CJBuchel <connorbuchel@gmail.com>
2019-01-15 00:18:23 +0800

    Made Better Changes
    Still doesn't enable Cameras

--- vision/src/main/cpp/vision.cpp
+++ vision/src/main/cpp/vision.cpp
@@ -46,35 +46,36 @@ float height_goal;
   
   void curtin_frc_vision::run() {
   // This creates a webcam on USB, and dumps it into a sink. The sink allows us to access the image with sink.GrabFrame
-  cs::UsbCamera cam{"USBCam", 1};
+  cs::UsbCamera cam{"USBCam", 0};
   cs::CvSink sink{"USB"};
   sink.SetSource(cam);
 
   // The camera defaults to a lower resolution, but you can choose any compatible resolution here.
   cam.SetResolution(640, 480);
 
-  		auto video_mode = cam.GetVideoMode();
-  		std::cout << "Width: " << video_mode.width << " Height: " << video_mode.height << std::endl;
+    auto video_mode = cam.GetVideoMode();
+  	std::cout << "Width: " << video_mode.width << " Height: " << video_mode.height << std::endl;
 
-  		// This lets us see the camera output on the robot dashboard. We give it a name, and a width and height.
-  		cs::CvSource output = frc::CameraServer::GetInstance()->PutVideo("USB Camera", video_mode.width, video_mode.height);
-  	
-  		cv::Mat imgOriginal{video_mode.height, video_mode.width, CV_8UC3};
-  		cv::Mat img_HSV{video_mode.height, video_mode.width, CV_8UC3};
+	cs::CvSource output = frc::CameraServer::GetInstance()->PutVideo("USB Camera", video_mode.width, video_mode.height);
+	cv::Mat imgOriginal{video_mode.height, video_mode.width, CV_8UC3};
+  	cv::Mat img_HSV{video_mode.height, video_mode.width, CV_8UC3};
+	cv::namedWindow("Original");
 
 
- while (true) {
-    	// Grab a frame. If it's not an error (!= 0), convert it to HSV and send it to the dashboard.
-    	if (sink.GrabFrame(imgOriginal) != 0) {
-      		cv::cvtColor(imgOriginal, img_HSV, cv::COLOR_BGR2HSV);
-    	}
-	
-		imshow("Original", imgOriginal); //Shows the original image
-	
+	while (true) {
+		
+		if (sink.GrabFrame(imgOriginal) != 0) {
+			imshow("Original", imgOriginal); //Shows the original image
+		}
+
+
 		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
 		{
 			cout << "esc key is pressed by user" << endl;
 			//break;
 		}
-  	}
+
+    }
+
+
 }
