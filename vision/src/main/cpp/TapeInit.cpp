1ff94fd7f343d722850cb643738370d6371c685f
CJBuchel <connorbuchel@gmail.com>
2019-01-15 09:32:28 +0800

    Started Splitting

--- vision/src/main/cpp/TapeInit.cpp
+++ vision/src/main/cpp/TapeInit.cpp
@@ -1,5 +1,45 @@
 #include <TapeInit.h>
+#include "Run.h"
+#include <opencv2/opencv.hpp>
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include <stdio.h>
+#include <iostream>
+
+#include <cameraserver/CameraServer.h>
+#include <networktables/NetworkTableInstance.h>
+#include <cscore.h>
+
+#include "devices/kinect.h"
+
+using namespace cv;
+using namespace std;\
 
 void curtin_frc_vision::TapeInit(){
 
+RNG rng(12345);
+Rect bounding_rect;
+int thresh = 100;
+float height_offset;
+float width_offset;
+float width_goal;
+float height_goal;
+
+// This is the main entrypoint into the CurtinFRC Vision Program!
+
+
+  // This is just a demonstration so you can see how this kind of code works. You'll be replacing this
+  // with our actual vision tracking software!
+  // You can view the vision output with Shuffleboard. Launch with `./gradlew :vision:ShuffleBoard`
+
+  vector<float> angles;
+  vector<cv::Point2f> centres;
+  vector<float> heights;
+  vector<bool> lefts;
+  vector<bool> rights;
+  vector<cv::Point2f> targets;
 }
