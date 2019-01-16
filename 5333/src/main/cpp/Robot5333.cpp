af325cca94b0f655bc6fe237fb68ae7661b4f26c
N.R.T <nathsharwor@hotmail.com>
2019-01-16 15:43:13 +0800

    Add cameras to SmartDashboard

--- 5333/src/main/cpp/Robot5333.cpp
+++ 5333/src/main/cpp/Robot5333.cpp
@@ -3,10 +3,15 @@
 #include <math.h>
 #include <iostream>
 
+#include "cameraserver/CameraServer.h"
+
 using namespace frc;
 using namespace curtinfrc;
 
 void Robot::RobotInit() {
+  CameraServer::GetInstance()->StartAutomaticCapture(0);
+  CameraServer::GetInstance()->StartAutomaticCapture(1);
+
   joy = new curtinfrc::Joystick(0);
   
   leftSRX = new TalonSrx(1);
@@ -27,14 +32,14 @@ void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {
-  double joyY = -joy->GetCircularisedAxisAgainst(joy->kYAxis, joy->kZAxis) * 0.6;
-  double joyZ = joy->GetCircularisedAxisAgainst(joy->kZAxis, joy->kYAxis) * 0.4;
+  double joyY = -joy->GetCircularisedAxisAgainst(joy->kYAxis, joy->kZAxis) * 0.9;
+  double joyZ = joy->GetCircularisedAxisAgainst(joy->kZAxis, joy->kYAxis) * 0.65;
+
+  joyY *= abs(joyY);
+  joyZ *= abs(joyZ);
 
   double leftSpeed = joyY + joyZ;
   double rightSpeed = joyY - joyZ;
-
-  leftSpeed *= abs(leftSpeed);
-  rightSpeed *= abs(rightSpeed);
 
   left->Set(leftSpeed);
   right->Set(rightSpeed);
