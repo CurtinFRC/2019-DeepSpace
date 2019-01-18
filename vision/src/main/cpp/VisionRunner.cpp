018e7fc351681e21260c14fc2b8dbc8f59cf1a32
Joseph Loveday <joseph.loveday2@gmail.com>
2019-01-18 15:28:12 +0800

    Sleep inside of thread to prevent pinning the CPU

--- vision/src/main/cpp/VisionRunner.cpp
+++ vision/src/main/cpp/VisionRunner.cpp
@@ -1,33 +1,13 @@
 #include "VisionRunner.h"
-#include "Capture.h"
-#include "TapeProcessing.h"
-#include "BallProcessing.h"
-#include "Display.h"
-
-#include <opencv2/opencv.hpp>
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/core/core.hpp"
-#include <stdio.h>
-#include <iostream>
-
-#include <cameraserver/CameraServer.h>
-#include <networktables/NetworkTableInstance.h>
-#include <cscore.h>
-
-#include "devices/kinect.h"
-
-using namespace cv;
-using namespace std;
+#include <chrono>
 
 // Handles threading
 void VisionRunner::Run(Runnable &run) {
     workers.push_back(std::thread([&]() {
       run.Init();
-      while (true)
+      while (true) {
         run.Periodic();
+        std::this_thread::sleep_for(std::chrono::duration<double>(1.0 / 120.0));
+      }
     }));
 }
--- vision/src/main/include/VisionRunner.h
+++ vision/src/main/include/VisionRunner.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <thread>
 #include <vector>
+
 #include "Runnable.h"
 
 class VisionRunner {
