7441f28d40a19e19d27d110903d71cb535e61874
Harry C <34387466+Hazzer890@users.noreply.github.com>
2019-01-23 16:57:03 +1100

    Lachlan added some MaAD code

--- 5663/src/main/cpp/Robot5663.cpp
+++ 5663/src/main/cpp/Robot5663.cpp
@@ -2,7 +2,6 @@
 #include <math.h>
 #include "Drivetrain.h"
 
-
 using namespace curtinfrc;
 using namespace frc;
 using hand = frc::XboxController::JoystickHand; // Type alias for hand
@@ -10,15 +9,16 @@ using hand = frc::XboxController::JoystickHand; // Type alias for hand
 
 void Robot::RobotInit() {
   // Motor_Controllers
-  leftTalon = new TalonSrx(1, 2048);
-  leftVictor = new VictorSpx(2);
+  leftTalon = new TalonSrx(2, 2048);
+  leftVictor = new VictorSpx(3);
   Left = new SensoredTransmission{ new SpeedControllerGroup(*leftTalon, *leftVictor), nullptr };
 
-  rightTalon = new TalonSrx(3, 2048);
+  rightTalon = new TalonSrx(5, 2048);
   rightVictor = new VictorSpx(4);
   Right = new SensoredTransmission{ new SpeedControllerGroup(*rightTalon, *rightVictor), nullptr };
 
-    rotateTalon = new TalonSrx(1, 2048);
+    rotateTalon1 = new TalonSrx(1, 2048);
+    rotateTalon2 = new TalonSrx(6, 0);
   DrivetrainConfig drivetrainConfig{*Left, *Right};
   drivetrain = new Drivetrain(drivetrainConfig);
 
@@ -41,14 +41,22 @@ void Robot::TeleopPeriodic() {
    double right_speed = xbox1->GetY(hand::kRightHand);
    drivetrain->Set(left_speed, right_speed);
   
-  
+  //flooper mech
+  double Hatch = xbox2->GetY(hand::kRightHand);
+  Hatch = std::abs(Hatch);      //lachlan's mad code
+  rotateTalon2->Set(Hatch);
+
+  //cargor mech
+  double cargo = xbox2->GetY(hand::kLeftHand);
+  cargo = std::abs(cargo);      //lachlan's mad code
+  rotateTalon1->Set(cargo);
+
   //Hatch Ejection
   if(xbox1->GetBumper(hand::kLeftHand) == 1){
     hatch_deploy1->frc::DoubleSolenoid::Set(frc::DoubleSolenoid::kForward);
     } else  { 
     hatch_deploy1-> frc::DoubleSolenoid::Set(frc::DoubleSolenoid::kReverse);
     }
-
 
   }
 
