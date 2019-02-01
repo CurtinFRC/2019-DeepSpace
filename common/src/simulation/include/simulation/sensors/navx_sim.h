566c3cfcd793b65cc3fcb1a63edebcb79859c8ca
Jaci Brunning <jaci.brunning@gmail.com>
2019-02-02 00:23:06 +0800

    Initialize NavX::Impl on simulation properly

--- common/src/simulation/include/simulation/sensors/navx_sim.h
+++ common/src/simulation/include/simulation/sensors/navx_sim.h
@@ -5,8 +5,8 @@
 namespace curtinfrc {
 namespace sensors {
   struct NavX::Impl {
-    std::array<double, 3> angles{0};
-    std::array<double, 3> angular_rates{0};
+    std::array<double, 3> angles{ {0, 0, 0} };
+    std::array<double, 3> angular_rates{ {0, 0, 0} };
   };
 }  // namespace sensors
 }  // namespace curtinfrc
