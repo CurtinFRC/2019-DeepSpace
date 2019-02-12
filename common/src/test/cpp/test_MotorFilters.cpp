770af218744260cb640483925f2e0932e525500c
Jaci Brunning <jaci.brunning@gmail.com>
2019-02-13 01:27:54 +0800

    Add tests for CurrentFFFilter

--- common/src/test/cpp/test_MotorFilters.cpp
+++ common/src/test/cpp/test_MotorFilters.cpp
@@ -23,7 +23,34 @@ class MockEncoder : public sensors::Encoder {
   double tick_vel = 0;
 };  
 
-TEST(AccelerationFilter, DoesLimit) {
+TEST(CurrentFFFilter, DoesLimit) {
+  auto motor = physics::DcMotor::mCIM();
+  MockEncoder encoder{};
+
+  Gearbox gb{ nullptr, &encoder, 1, motor };
+
+  // Limits: -57 A (@ 3090 RPM), 82 Nm (@ 2025 RPM)
+  control::CurrentFFFilter filter{-57, 82, gb};
+
+  double speed_min = -3090 / 60.0 * 2 * 3.1415;
+  double speed_max = 2025 / 60.0 * 2 * 3.1415;
+
+  // We are still, so all voltage is being used to accelerate.
+  encoder.SetVelocity(0);
+  EXPECT_LT(filter.Get(12.0), 12.0);
+  
+  // We are moving, so most of our voltage is being used to hold velocity.
+  encoder.SetVelocity(speed_max * 1.1);
+  EXPECT_DOUBLE_EQ(filter.Get(12.0), 12.0);
+
+  encoder.SetVelocity(0);
+  EXPECT_GT(filter.Get(-12.0), -12.0);
+
+  encoder.SetVelocity(speed_min * 1.1);
+  EXPECT_DOUBLE_EQ(filter.Get(-12.0), -12.0);
+}
+
+TEST(AccelerationFFFilter, DoesLimit) {
   auto motor = physics::DcMotor::mCIM();
   MockEncoder encoder{};
 
